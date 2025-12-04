2a54e6c1d7ddde9e903f071629d4293e2ba10a3d
saumya <saumya.dixit@rapidops.com>
2025-12-04 21:46:14 +0530

    added env variables to react

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,9 +1,9 @@
 export const ENDPOINTS = {
-  'get-oauth-link-google': 'http://127.0.0.1:4040/oauth/oauthlink/google',
-  'get-oauth-link-spotify': 'http://127.0.0.1:4040/oauth/oauthlink/spotify',
-  'authenticate-google': 'http://127.0.0.1:4040/oauth/authenticate/google',
-  'authenticate-spotify': 'http://127.0.0.1:4040/oauth/authenticate/spotify',
-  'convert-youtube-playlist': 'http://127.0.0.1:4040/playlist/convert-youtube-playlist/:userId',
-  'user-token-status': 'http://127.0.0.1:4040/user/user-token-status'
+  'get-oauth-link-google': process.env.REACT_APP_BACKEND_ENDPOINT +'/oauth/oauthlink/google',
+  'get-oauth-link-spotify': process.env.REACT_APP_BACKEND_ENDPOINT +'/oauth/oauthlink/spotify',
+  'authenticate-google': process.env.REACT_APP_BACKEND_ENDPOINT +'/oauth/authenticate/google',
+  'authenticate-spotify': process.env.REACT_APP_BACKEND_ENDPOINT +'/oauth/authenticate/spotify',
+  'convert-youtube-playlist': process.env.REACT_APP_BACKEND_ENDPOINT +'/playlist/convert-youtube-playlist/:userId',
+  'user-token-status': process.env.REACT_APP_BACKEND_ENDPOINT +'/user/user-token-status'
 }
 
