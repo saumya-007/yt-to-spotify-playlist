0160cc66dc765712d129b27f2b4c6dd808c5bb18
saumya <saumya.dixit@rapidops.com>
2025-12-04 20:50:24 +0530

    added auth creds file

---
+++ backend/oauth-creeds-google.js
@@ -0,0 +1,7 @@
+module.exports = Object.freeze({
+  clientId: process.env.clientIdGoogle,
+  clientSecret: process.env.clientSecretGoogle,
+  redirectUrl: process.env.redirectUrlGoogle,
+  rootUrlForAccessToken: process.env.rootUrlForAccessTokenGoogle,
+  rootUrl: process.env.rootUrlGoogle,
+})
---
+++ backend/oauth-creeds-spotify.js
@@ -0,0 +1,7 @@
+module.exports = Object.freeze({
+  clientId: process.env.clientIdSpotify,
+  clientSecret: process.env.clientSecretSpotify,
+  redirectUrl: process.env.redirectUrlSpotify,
+  rootUrlForAccessToken: process.env.rootUrlForAccessTokenSpotify,
+  rootUrl: process.env.rootUrlSpotify,
+})
